0679c9edae99e39beff4980ba2fca378bb159277
mariana <vergaramariana976@gmail.com>
2022-07-21 15:17:30 -0300

    mari cambios

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 
 
@@ -9,6 +10,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>hola mundoX!</Text>
       <StatusBar style="auto" />
+      <LoginForm></LoginForm>
     </View>
   );
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,8 @@
 import React from "react";
-import { View, Checkbox, TextInput, View, Button } from "react-native"
+import { Text, TextInput, Button, View} from "react-native"
 
 export default function LoginForm(){
-    return(
-        <view>
-            <TextInput placeholder="email"/>
-            <TextInput placeholder="password"/>
-            <Button title="aceptar" onPress={()=>console.log("enviar")}/>
-            <Button title="aceptar" onPress={()=>console.log("cancelar")}/>
-            
-        </view>
+    return (
+        
     )
 }
