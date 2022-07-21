578acdf0c6e49859f2cd536b1038589349cda4f8
mariana <vergaramariana976@gmail.com>
2022-07-21 15:23:14 -0300

    empezando formulario

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Text, TextInput, Button, View} from "react-native"
+import { TextInput, Button, View} from "react-native"
 
 export default function LoginForm(){
     return (
-        
-    )
+        <View> 
+            <TextInput placeholder="Email"/>
+            <TextInput placeholder="Password"/>
+            <Button title="Aceptar" onPress={() => console.log("Enviar")}/>
+            <Button title="Cancelar" onPress={() => console.log("Cancelar")}/>
+        </View>
+
+
+)
 }
