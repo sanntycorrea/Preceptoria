38b32acaf366e869619a4ea95f98508c907e13a6
mariana <vergaramariana976@gmail.com>
2022-07-21 15:26:56 -0300

    santi mas cambios

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import LoginForm from './src/components/LoginForm';
 
 
@@ -8,7 +8,6 @@ import LoginForm from './src/components/LoginForm';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hola mundoX!</Text>
       <StatusBar style="auto" />
       <LoginForm></LoginForm>
     </View>
