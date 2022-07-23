392b2d626ce0da98a9cf7da53874470289256d79
mariana <vergaramariana976@gmail.com>
2022-07-23 20:24:09 -0300

    pruebas form mary

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { TextInput, Button, View} from "react-native"
+import { TextInput, View, Checkbox} from "react-native"
 
 export default function LoginForm(){
     return (
         <View> 
-            <TextInput placeholder="Email"/>
-            <TextInput placeholder="Password"/>
-            <Button title="Aceptar" onPress={() => console.log("Enviar")}/>
-            <Button title="Cancelar" onPress={() => console.log("Cancelar")}/>
+            <TextInput placeholder="Nombre"></TextInput>
+        <TextInput placeholder="Apellido"></TextInput>
+        <TextInput placeholder="Nombre Usuario"></TextInput>
+        <TextInput placeholder="Correo"></TextInput>
+        <TextInput placeholder="Contraseña"></TextInput>
+        <Checkbox placeholder="Acepta los terminos y condiciones"></Checkbox>
+        <Checkbox placeholder="Mantener Sesion"></Checkbox>
         </View>
-asd
+
 
 )
 }
